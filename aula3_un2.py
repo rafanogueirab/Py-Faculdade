7d6276eff9f5c2d5187580699fcf23737bad59c7
Rafael Nogueira <contato.rafaelnogueiraoficial@gmail.com>
2025-09-16 12:41:50 -0300

    Create aula3_un2.py

---
+++ aula3_un2.py
@@ -0,0 +1,27 @@
+class Pessoa:
+    #o código __init__ é um construtor, chamado quando um objeto da classe é criado.
+    #ele inicializa os atributos da classe.
+    def __init__(self, nome, idade, genero):
+        self.nome = nome
+        self.idade = idade
+        self.genero = genero
+        #self é uma convenção em Python que se refere à própria instância da classe.
+        #Os parâmetros nome, idade e genero são passados durante a criação do objeto.
+        # Eles são usados para inicializar os atributos da instância.
+
+    # O comando cumprimentar retorna uma saudação com o nome da pessoa
+    def cumprimentar(self):
+        return f"Olá, meu nome é {self.nome}"
+    # O comando aniversario aumenta a idade da pessoa em 1.
+    def aniversario(self):
+        self.idade += 1
+# Cria uma instância da classe "Pessoa" com os valores "João", 30 e "Masculino" para nome, idade e genero, respectivamente.
+pessoa1 = Pessoa('João', 30, 'Masculino')
+#Chama o comando "cumprimentar" na instância pessoa1 e imprime a saudação
+print(pessoa1.cumprimentar()) #Saída: Olá, meu nome é João
+#Acessa o atributo idade da instância pessoa1 e imprime sua idade
+print(f"Idade: {pessoa1.idade}")
+#Chama o comando aniversario na instância pessoa1 para aumentar sua idade em 1
+pessoa1.aniversario()
+#Acessa o atributo idade atualizada da instância pessoa1 e imprime sua nova idade.
+print(f"Nova Idade: {pessoa1.idade}")
