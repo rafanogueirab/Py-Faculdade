6cd5b7170a6c2820e23edd020b5392df8a170436
Rafael Nogueira <contato.rafaelnogueiraoficial@gmail.com>
2025-09-24 11:10:44 -0300

    Create aula2_un3(1).py

---
+++ aula2_un3(1).py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+#Criando um dicionário com pares chaves-valor
+exemplo2 = {'A': 100, 'B': 200, 'C': 300, 'D': 400, 'E': 500}
+
+#Criando uma Series a partir do dicionário
+series2 = pd.Series(data = exemplo2)
+
+print(series2)
