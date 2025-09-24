2ba4075ca8fc281481e3b4309f696c44e8537541
Rafael Nogueira <contato.rafaelnogueiraoficial@gmail.com>
2025-09-23 21:20:48 -0300

    update nova aula

---
+++ aula1_un3(1).py
@@ -0,0 +1,16 @@
+#Adicionar produto
+import sqlite3
+#Conectando ao banco de dados
+conn = sqlite3.connect('exemplo.db')
+cursor = conn.cursor()
+#Dados do novo produto
+novo_produto = ('Camiseta', 19.99, 50)
+#Comando SQL para inserir o novo produto na tabela
+inserir_produto = "INSERT INTO Produtos (nome, preco, estoque) VALUES (?, ?, ?)"
+#Executando o comando SQL para inserção
+cursor.execute(inserir_produto, novo_produto)
+#Confirmando as alterações
+conn.commit()
+#Fechando a conexão
+conn.close()
+
